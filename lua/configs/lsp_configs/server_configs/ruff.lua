9fab7c50b13c6cd046b5e59b69492c580e6d93d9
Mirei124 <chkycc@gmail.com>
2024-12-28 13:48:19 +0800

    change ruff extendSelect

--- lua/configs/lsp_configs/server_configs/ruff.lua
+++ lua/configs/lsp_configs/server_configs/ruff.lua
@@ -3,6 +3,10 @@ return {
   init_options = {
     settings = {
       lineLength = 120,
+      lint = {
+        -- https://docs.astral.sh/ruff/rules/
+        extendSelect = { "D2", "D3", "D4" },
+      },
     }
   }
 }
