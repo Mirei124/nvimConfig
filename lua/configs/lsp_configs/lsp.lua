66f3efcec2a0ba4e1e14123b9b44656529158f25
Connor <chkycc@qq.com>
2025-04-26 20:13:47 +0800

    fix: lsp diagnostic config

--- lua/configs/lsp_configs/lsp.lua
+++ lua/configs/lsp_configs/lsp.lua
@@ -24,9 +24,12 @@ return function()
     }
   })
 
-  -- Set icons for sidebar.
+  -- :h vim.diagnostic.Opts
   local icons = require("utils.icons").get("diagnostics", true)
   vim.diagnostic.config({
+    underline = { severity = vim.diagnostic.severity.ERROR },
+    virtual_text = { severity = vim.diagnostic.severity.ERROR },
+    virtual_lines = false,
     signs = {
       text = {
         [vim.diagnostic.severity.ERROR] = icons.Error_alt,
@@ -34,39 +37,13 @@ return function()
         [vim.diagnostic.severity.INFO] = icons.Information_alt,
         [vim.diagnostic.severity.HINT] = icons.Hint_alt,
       },
-      -- linehl = {
-      --   [vim.diagnostic.severity.ERROR] = "DiagnosticSignError",
-      --   [vim.diagnostic.severity.WARN] = "DiagnosticSignWarn",
-      --   [vim.diagnostic.severity.INFO] = "DiagnosticSignInfo",
-      --   [vim.diagnostic.severity.HINT] = "DiagnosticSignHint",
-      -- },
-    }
+    },
+    update_in_insert = false,
+    severity_sort = true,
   })
 
   --- lspconfig --------------------------------------------
   local lspconfig = require("lspconfig")
-
-  -- :h lsp-handler-configuration
-  vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
-    vim.lsp.diagnostic.on_publish_diagnostics, {
-      -- :h vim.diagnostic.config()
-      underline = {
-        -- set to HINT for pyright to display gray out text
-        severity = vim.diagnostic.severity.HINT,
-      },
-      virtual_text = {
-        severity = vim.diagnostic.severity.ERROR,
-      },
-      signs = true,
-      float = {
-        style = "minimal",
-        border = "single",
-        focusable = false,
-      },
-      update_in_insert = false,
-      severity_sort = true,
-    }
-  )
 
   -- :h vim.lsp.ClientConfig
   local opts = {
