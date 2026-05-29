da6bebfdb5baa34f1d484d805cb77fa19c9bd247
Mirei124 <chkycc@qq.com>
2026-05-29 19:56:40 +0800

    Add gitcommit autocmd

--- lua/autocmd.lua
+++ lua/autocmd.lua
@@ -45,3 +45,10 @@ vim.api.nvim_create_autocmd("BufWritePre", {
     require("utils.funcs").lsp_formatting(false)
   end,
 })
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "gitcommit",
+  callback = function()
+    vim.opt_local.spell = true
+  end,
+})
